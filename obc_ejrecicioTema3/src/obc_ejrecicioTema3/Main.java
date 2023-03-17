bb9d8cbf175464a104d7074154ee988b6230ca3a
Jose Soto <jose.soto.developer@gmail.com>
2023-03-17 14:47:53 +0100

    commit final

--- obc_ejrecicioTema3/src/obc_ejrecicioTema3/Main.java
+++ obc_ejrecicioTema3/src/obc_ejrecicioTema3/Main.java
@@ -4,15 +4,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		//Primera parte
 		System.out.println(funcion(1,2,3));
 		System.out.println(funcion(10, 15, 20));
 		
-		Coche cocheUno = new Coche();
-		System.out.println("update");
 		
+		//Segunda parte
+		Coche cocheUno = new Coche(4);
+		int numeroPuertas = cocheUno.incrementoPuerta();
+		
+		System.out.println(numeroPuertas);
 		
 	}
 	
+	//Primera parte
 	public static int funcion(int a, int b, int c) {
 		int resultado = a + b + c;
 		return resultado;
@@ -22,8 +27,14 @@ public class Main {
 	
 }
 
+//Segunda parte
 class Coche{
 	
+	public Coche(int puerta) {
+		super();
+		this.puerta = puerta;
+	}
+
 	int puerta;
 	
 	public int incrementoPuerta() {
